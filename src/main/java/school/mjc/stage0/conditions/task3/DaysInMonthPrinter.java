7f42b73a5414db6df0d3252bf1ba82d20a5c4615
Kritskaya Tatyana <kritskaya.tatjana@gmail.com>
2023-12-20 16:40:02 +0300

    fix: add condition rule

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
-        if (month > 12) {
+        if (month > 12 || month < 1) {
             System.out.println("wrong number!");
         } else if (month == 2) {
             System.out.println(28);
